1dcac59a6883471c813f8fe1ca6a7afb45a26679
Your Name <you@example.com>
2019-06-14 17:02:00 -0300

    removed readfiles returns....

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ res.send(data);
   fs.readdir(dirname, function(err, filenames) {
     if (err) {
       onError(err);
-      return;
+      //return;
     }
     filenames.forEach(function(filename) {
       fs.readFile(dirname + '/' + filename, 'utf-8', function(err, content) {
         if (err) {
           onError(err);
-          return;
+          //return;
         }
         onFileContent(filename, content);
       });
